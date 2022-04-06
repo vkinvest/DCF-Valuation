8b12a01e1fc469dd011b8d079fee14ff63c8d719
vkinvest <vkyinvest@gmail.com>
2022-04-06 00:12:56 -0400

    Update

--- Stock Screener/stock comparision.py
+++ Stock Screener/stock comparision.py
@@ -6,22 +6,28 @@ import statistics
 import FundamentalAnalysis as fa
 import time
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.io as pio
+import plotly.offline as ply
+import plotly.graph_objs as go
+import chart_studio.plotly as py
 
 
-
-
-
-tickers = ['FSLY', 'TSLA', 'NIO']
+tickers = ['FSLY', 'TSLA', 'NIO', 'RUN', 'PARA', 'QS', 'FSLY', 'JMIA', 'BARK', 'SUNW']
 period = 'annual'
 api_key = 'e95137f175e3fba84a1220c74e5ecd2a'
 
-begin = "2021-08-18"
+begin = "2022-01-01"
 end = dt.date.today()
 
 statements = {}
 revenue = pd.DataFrame()
 growth = pd.DataFrame()
 price = pd.DataFrame()
+volume = pd.DataFrame()
+volume_mean = {}
+volume_trend = pd.DataFrame()
+volume_attention = pd.DataFrame()
 
 for ticker in tickers:
     statements[ticker] = {
@@ -35,12 +41,29 @@ for ticker in tickers:
     growth[ticker] = revenue[ticker].diff() / revenue[ticker].shift(1)
 
     price[ticker] = fa.stock_data_detailed(ticker, api_key, begin=begin, end=end)["adjClose"].sort_index()
-    price_change = price / price.iloc[0] * 100
 
-growth.plot(figsize=(12, 6))
-plt.title(f'Revenue growth trend')
+    volume[ticker] = fa.stock_data_detailed(ticker, api_key, begin=begin, end=end)['volume'].sort_index()
+    volume_mean[ticker] = statistics.mean(volume[ticker])
+    volume_trend[ticker] = volume[ticker] / volume_mean[ticker]
+    volume_attention[ticker] = volume_trend[ticker].loc[volume_trend[ticker] > 1]
+
+volume_list = []
+for col in volume_attention.columns:
+    plot_trend = go.Scatter(
+        name=col,
+        x=volume_attention.index,
+        y=volume_attention[col]
+    )
+    volume_list.append(plot_trend)
+
+volume_fig = go.Figure(data=volume_list)
+volume_fig.update_layout(title='Volume Above Average')
+py.plot(volume_fig)
+
+
+growth.plot(figsize=(12, 6), title='Revenue growth trend')
 plt.show()
 
-price_change.plot(figsize=(12, 6))
-plt.title(f'Price trend since {begin}')
+price_change = price / price.iloc[0] * 100
+price_change.plot(figsize=(12, 6), title='Price trend since {begin}')
 plt.show()
